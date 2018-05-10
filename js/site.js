d393d3cc9ff55420c85ab6d9205b3e1eb9199a0d
Bryan Stoner <bryan@fastspot.com>
2018-05-10 16:57:52 -0400

    spotlight

--- js/site.js
+++ js/site.js
@@ -955,3 +955,43 @@ Site.modules.Now = (function($, Site) {
 	return {};
 
 })(jQuery, Site);
+
+/*-------------------------------------------
+	Spotlight
+-------------------------------------------*/
+
+Site.modules.Spotlight = (function($, Site) {
+
+	function init() {
+		if ($(".spotlight").length) {
+			bindUI();
+		}
+	}
+
+	function bindUI() {
+		$(".spotlight_item").hover(function() {
+			$(".spotlight").addClass("interested");
+		}, function() {
+			$(".spotlight").removeClass("interested");
+		});
+
+		$(".spotlight_video_trigger").on("click", updateVideo);
+	}
+
+	function updateVideo() {
+		if(!($(".spotlight_video_trigger").hasClass("paused"))) {
+			$(".spotlight_body_background").background("pause");
+			$(".spotlight_video_trigger").addClass("paused");
+			$(".spotlight_video_label").html("Play Video");
+		} else {
+			$(".spotlight_body_background").background("play");
+			$(".spotlight_video_trigger").removeClass("paused");
+			$(".spotlight_video_label").html("Pause Video");
+		}
+	}
+
+	Site.onInit.push(init);
+
+	return {};
+
+})(jQuery, Site);
