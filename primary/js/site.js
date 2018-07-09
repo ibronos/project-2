412586a92b12ead607eb4011cb6a2235d406b2d6
Bryan Stoner <bryan@fastspot.com>
2018-07-09 12:02:21 -0400

    remove iframes on close

--- primary/js/site.js
+++ primary/js/site.js
@@ -1097,12 +1097,20 @@ Site.modules.Video = (function($, Site) {
 
 	function bindUI() {
 		$(".video_item_video, .quote_item_video").on("click", insertVideo);
+
+		$(".media_gallery_close").on("click", removeVideos);
 	}
 
 	function insertVideo() {
 		var video = $(this).data("url");
 
 		$(this).after('<iframe class="video_item_iframe" src="' + video + '?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>');
+	}
+
+	function removeVideos() {
+		$(".media_gallery_item").each(function() {
+			$(this).find("iframe").remove();
+		});
 	}
 
 	Site.onInit.push(init);
