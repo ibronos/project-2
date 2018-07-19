c20d1f6ac4195fdcae4ab8f46b47e5b2c16826a3
Dharmawan Sukma <waone.on1@gmail.com>
2018-07-19 11:35:04 +0700

    Video - Programs should allow optional video - Homepage People/Places/Pride Story Videos

--- primary/inc/partials/homepage-programs.php
+++ primary/inc/partials/homepage-programs.php
@@ -21,8 +21,8 @@ $programs_post = get_posts( array(
 	<div class="spotlight_takeover_intro">
 		<div class="spotlight_takeover_intro_inner">
 			<div class="js-background spotlight_takeover_intro_background" data-background-options='{"source": {
-			"740px": "//images.fastspot.com/trinity-redesign/980x654/1",
-			"980px": "//images.fastspot.com/trinity-redesign/1220x686/1"
+			"740px": "<?php echo get_field($item)['image'] ?>",
+			"980px": "<?php echo get_field($item)['image'] ?>"
 			}}'></div>
 			<header class="spotlight_takeover_intro_header">
 				<h3 class="spotlight_takeover_intro_label"><?php echo $item ?></h3>
@@ -69,50 +69,79 @@ $programs_post = get_posts( array(
 
 		<?php foreach ($programs_post as $post): ?>
 			<?php setup_postdata( $post ) ?>
-			<?php
-				$t_types = wp_get_post_terms($post->ID, 'areas-of-study', array("fields" => "all"));
-			?>
+			<?php $t_types = wp_get_post_terms($post->ID, 'areas-of-study', array("fields" => "all")); ?>
+
 			<article class="program <?php foreach($t_types as $key => $value){echo $value->slug.' ';} ?>">
-				<picture class="program_picture">
-					<source media="(min-width: 740px)" srcset="<?php the_field('image') ?>" />
-					<source media="(min-width: 0px)" srcset="<?php the_field('image') ?>" />
-					<img class="program_image" src="<?php the_field('image') ?>" alt="">
-				</picture>
-				<div class="program_body">
-					<header class="program_header">
-						<h3 class="program_title">
-							<a class="program_title_link" target="_blank" href="<?php the_field('link') ?>">
-								<span class="program_title_link_label"><?php the_title() ?></span>
-								<span class="program_title_link_icon" aria-hidden="true">
-									<svg class="icon icon_arrow_right">
-										<use xlink:href="<?php echo tric_icon('arrow_right') ?>"></use>
-									</svg>
-								</span>
-							</a>
-						</h3>
-						<div class="program_details">
-							<span class="program_label">Degree(s) Offered</span>
-							<span class="program_degrees">
-								<?php foreach (get_field('degrees_offered') as $i => $degrees_offered): ?>
-									<span class="program_degree">
-										<?php echo ($i > 0) ? ',' : '' ?>
-										<?php echo $degrees_offered ?>
+				<?php if (get_field('video_url')): ?>
+					<div class="video_item">
+						<figure class="video_item_figure">
+							<picture class="video_item_picture">
+								<source media="(min-width: 740px)" srcset="<?php the_field('image') ?>" />
+								<source media="(min-width: 0px)" srcset="<?php the_field('image') ?>" />
+								<img class="video_item_image" src="<?php the_field('image') ?>" alt="">
+							</picture>
+							<button class="video_item_video" title="play video" data-url="<?php echo tric_video_support::render_video(get_field('video_url')) ?>">
+								<span class="video_item_video_hint">
+									<span class="video_item_video_hint_icon_bubble">
+										<span class="video_item_video_hint_icon">
+											<svg class="icon icon_play">
+												<use xlink:href="<?php tric_icon('play') ?>"></use>
+											</svg>
+										</span>
 									</span>
-								<?php endforeach ?>
-							</span>
+									<span class="video_item_video_hint_label">Watch the Video</span>
+								</span>
+							</button>
+						</figure>
+						<div class="video_item_body">
+							<header class="video_item_header">
+								<h3 class="video_item_title"><?php the_title() ?></h3>
+							</header>
 						</div>
-					</header>
-					<div class="program_caption"><?php the_field('blurb') ?></div>
-					<a class="program_link" target="_blank" href="<?php the_field('link') ?>">
-						<span class="program_link_label">Learn More</span>
-						<span class="program_link_icon" aria-hidden="true">
-							<svg class="icon icon_arrow_right">
-								<use xlink:href="<?php echo tric_icon('arrow_right') ?>"></use>
-							</svg>
-						</span>
-					</a>
-				</div>
+					</div>
+				<?php else: ?>
+					<picture class="program_picture">
+						<source media="(min-width: 740px)" srcset="<?php the_field('image') ?>" />
+						<source media="(min-width: 0px)" srcset="<?php the_field('image') ?>" />
+						<img class="program_image" src="<?php the_field('image') ?>" alt="">
+					</picture>
+					<div class="program_body">
+						<header class="program_header">
+							<h3 class="program_title">
+								<a class="program_title_link" target="_blank" href="<?php the_field('link') ?>">
+									<span class="program_title_link_label"><?php the_title() ?></span>
+									<span class="program_title_link_icon" aria-hidden="true">
+										<svg class="icon icon_arrow_right">
+											<use xlink:href="<?php echo tric_icon('arrow_right') ?>"></use>
+										</svg>
+									</span>
+								</a>
+							</h3>
+							<div class="program_details">
+								<span class="program_label">Degree(s) Offered</span>
+								<span class="program_degrees">
+									<?php foreach (get_field('degrees_offered') as $i => $degrees_offered): ?>
+										<span class="program_degree">
+											<?php echo ($i > 0) ? ',' : '' ?>
+											<?php echo $degrees_offered ?>
+										</span>
+									<?php endforeach ?>
+								</span>
+							</div>
+						</header>
+						<div class="program_caption"><?php the_field('blurb') ?></div>
+						<a class="program_link" target="_blank" href="<?php the_field('link') ?>">
+							<span class="program_link_label">Learn More</span>
+							<span class="program_link_icon" aria-hidden="true">
+								<svg class="icon icon_arrow_right">
+									<use xlink:href="<?php echo tric_icon('arrow_right') ?>"></use>
+								</svg>
+							</span>
+						</a>
+					</div>
+				<?php endif ?>
 			</article>
+
 		<?php endforeach ?>
 		<?php wp_reset_postdata() ?>
 
